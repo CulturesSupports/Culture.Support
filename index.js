0ba973ad31cd1140d55a0aff7bbe7b2c15100243
Culture.Support <huxoz@protonmail.com>
2024-12-02 04:37:20 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-// Example of a simple form submission handling (to be connected with a server-side script)
-// const form = document.querySelector('form');
-// form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     // Add your AJAX submission logic here
-// });
